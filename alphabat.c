ef8a987032ee246f3b149f71b64a66dd1fabea5b
Sujal Visavaliya <150358295+fsd-sujal@users.noreply.github.com>
2023-12-07 05:32:13 -0800

    Add files via upload

---
+++ alphabat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+	char alphabat='a';
+	
+	do{
+		printf("%c\t",alphabat);
+		alphabat++;
+	}while(alphabat<='z');
+}
---
+++ table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int n,i;
+	printf("enter the number=");
+	scanf("%d",&n);
+	
+	printf("This is a table \n\n");
+	
+	for(i=1;i<=10;i++){
+		printf("%d*%d=%d\n",n,i,n*i);
+	}
+}
